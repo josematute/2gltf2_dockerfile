73300a8a0c125f549431e9d649673db6e89d06ff
Jose E Matute <60519538+josematute@users.noreply.github.com>
2024-04-08 22:57:18 -0600

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -11,12 +11,16 @@ input_file="/data/$1"
 
 # Extract the filename without the path
 filename=$(basename -- "$1")
+filename="${filename%.*}"
 
 # Change to the script's directory
 cd /opt/2gltf2
 
 # Execute the conversion
 ./2gltf2.sh "$input_file"
+
+# Move the GLTF file to the output directory
+mv "/opt/2gltf2/$filename.gltf" "/data/output/$filename.gltf"
 
 # No need to move the file, assuming the script outputs to the same directory as the input file
 echo "Check the current directory for the output file."
