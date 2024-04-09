d5a2295943bb3f4c4f9f27c7da2bbeb65faa8688
Jose E Matute <60519538+josematute@users.noreply.github.com>
2024-04-09 13:31:14 -0600

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Check if a filename has been provided
-if [ -z "$1" ]; then
-    echo "No file specified."
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 filename [output_path]"
+    echo "filename: Name of the file to convert. Must be located in /data directory."
+    echo "output_path: Optional. The path where the output files will be stored relative to /data. Defaults to /data/output."
+}
+
+# Check if a filename has been provided or if help is requested
+if [ -z "$1" ] || [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+    usage
     exit 1
 fi
 
@@ -19,11 +26,22 @@ cd /opt/2gltf2
 # Execute the conversion
 ./2gltf2.sh "$input_file"
 
-# Ensure the output directory exists in /data
-mkdir -p "/data/output/$filename"
+# Default output directory inside the container
+output_dir="/data/output"
 
-# Move the entire output directory to /data/output
-# This will transfer all converted files and associated resources
-mv "/opt/2gltf2/$filename" "/data/output/"
+# If a second argument is provided and it does not start with "/data", prepend "/data"
+if [ ! -z "$2" ]; then
+    if [[ "$2" == /data* ]]; then
+        output_dir="$2"
+    else
+        output_dir="/data/$2"
+    fi
+fi
 
-echo "Check /data/output/$filename for the output files."
+# Ensure the output directory exists
+mkdir -p "$output_dir/$filename"
+
+# Move the entire output directory to the specified or default output directory
+mv "/opt/2gltf2/$filename" "$output_dir/"
+
+echo "Check $output_dir/$filename for the output files."
